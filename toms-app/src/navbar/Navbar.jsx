056ba64baef51aef043f86c110384741b31696d4
thomas <tom_tyte@hotmail.com>
2024-11-28 17:36:08 -0500

    updated dom and navbar style

--- toms-app/src/navbar/Navbar.jsx
+++ toms-app/src/navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <ul className="navbar-list">
                 <li className="navbar-item">
-                    <Link to="/todos">Todos</Link>
+                    <span>Todos</span> {}
                 </li>
                 <li className="navbar-item">
-                    <Link to="/contact">Contact</Link>
+                    <span>Contact</span> {}
                 </li>
             </ul>
         </nav>
