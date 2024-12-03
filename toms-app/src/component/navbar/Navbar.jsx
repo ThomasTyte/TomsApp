f930d092be23d36188bceb83e677ee5c4d01e0da
thomas <tom_tyte@hotmail.com>
2024-12-03 00:29:08 -0500

    updated navbar

--- toms-app/src/component/navbar/Navbar.jsx
+++ toms-app/src/component/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ onHomeClick, onContactClick }) => {
+const Navbar = () => {
     return (
         <nav className="navbar">
             <ul className="navbar-list">
-                <li className="navbar-item" onClick={onHomeClick}>
-                    <span>Home</span>
+                <li className="navbar-item">
+                    <Link to="/">Home</Link>
                 </li>
-                <li className="navbar-item" onClick={onContactClick}>
-                    <span>Contact</span>
+                <li className="navbar-item">
+                    <Link to="/contact">Contact</Link>
                 </li>
             </ul>
         </nav>
