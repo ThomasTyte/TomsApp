25962cd3f21f545e431b14d78d5ab72145848bc6
thomas <tom_tyte@hotmail.com>
2024-12-03 01:21:41 -0500

    updated app.js without router dom

--- toms-app/src/App.js
+++ toms-app/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
 import Navbar from './component/navbar/Navbar';
 import Contact from './pages/contactpage/Contact'; 
+import TodoList from './pages/todolist/TodoList';
 import Home from './pages/Home/Home'; 
-import TodoList from './path/to/TodoList'; 
 
 function App() {
+    const [currentPage, setCurrentPage] = useState('home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'home':
+                return <Home />;
+            case 'contact':
+                return <Contact />;
+            case 'todolist':
+                return <TodoList />;
+            default:
+               
+        }
+    };
+
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />        
-                <Route path="/contact" element={<Contact />} />  
-                <Route path="/todolist" element={<TodoList />} /> 
-            </Routes>
-        </Router>
+        <div>
+            <Navbar onPageChange={setCurrentPage} />
+            {renderPage()}
+        </div>
     );
 }
 
